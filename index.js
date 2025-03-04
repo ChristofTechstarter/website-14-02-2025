5883ea006768f3508f7f2d101e6d05cab03cf5bf
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-03-04 13:41:57 +0100

    changed buttons

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function changeText() {
     const topic = document.getElementById("topic1")
     topic.style.color = "red";
     topic.innerHTML = "Du Hast den Button geklickt!!!"
-  }
+}
 
 function deleteText() {
   const topic = document.getElementById("topic1")
