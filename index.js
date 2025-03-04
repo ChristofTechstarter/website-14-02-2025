b62d0a8f0ddfff567635b269e716485243af0e12
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-03-04 12:49:32 +0100

    added new Button!

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function changeText() {
     topic.style.color = "red";
     topic.innerHTML = "Du Hast den Button geklickt!!!"
   }
+
+function deleteText() {
+  const topic = document.getElementById("topic1")
+  topic.style.color = "rgba(135, 206, 250, 0.200)";
+  topic.innerHTML = "Willkommen!";
+}
